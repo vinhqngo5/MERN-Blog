c25edbcccae9da6e898a79420a35d41b5afafe1f
vinhqngo5 <vinhqngo5@gmail.com>
2021-09-07 16:39:01 +0700

    Init frontend, backend and connect to mongodb atlas

---
+++ server/controllers/posts.js
@@ -0,0 +1,22 @@
+import { PostModel } from "../model/PostModel.js";
+export const getPosts = async (req, res) => {
+	try {
+		const posts = await PostModel.find();
+		console.log("🚀 ~ file: posts.js ~ line 5 ~ getPosts ~ posts", posts);
+		res.status(200).json(posts);
+	} catch (err) {
+		res.status(500).json({ error: err });
+	}
+};
+
+export const createPost = async (req, res) => {
+	try {
+		const newPost = req.body;
+		const post = new PostModel(newPost);
+		await post.save();
+		res.status(200).json(posts);
+	} catch (err) {
+		res.status(500).json({ error: err });
+	}
+};
+
---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import posts from "./routers/posts.js";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = process.env.port || 5000;
+
+const URI =
+	"mongodb+srv://admin:admin@cluster0.lfrsu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+// limit maximum of req from client to server
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+
+// providing a Connect/Express middleware to enable CORS with various options
+app.use(cors());
+
+// HTTP loggers
+app.use(morgan("combined"));
+
+// connect mongoose
+mongoose
+	.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("Connect to mongoose");
+	})
+	.catch((err) => {
+		console.error(err);
+	});
+
+app.use("/posts", posts);
+
+app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
